359ad3ffd2148dfbad7f12e0eada03f331613305
diyaag2004 <130734531+diyaag2004@users.noreply.github.com>
2023-04-16 01:50:45 +0530

    d

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const {
     loginController,
     registerController,
+    applyDoctorController,
 } =require("../controllers/userCtrl");
 //router onject 
 const router = express.Router();
@@ -11,7 +12,11 @@ router.post("/login",loginController);
 // REGISTER||POST
 router.post("/register", registerController);
 //Auth || POST
-router.post('/getUserData', authMiddleware ,  authController)
+router.post('/getUserData', authMiddleware ,  authController);
 module.exports =router;
+//Apply Doctor || POST
+router.post('/apply-doctor', authMiddleware ,  applyDoctorController);
+module.exports =router;
+
 
 
