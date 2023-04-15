da3e8377b940d713ef5a1f9e98c0c049a4f77271
diyaag2004 <diyaag2020@gmail.com>
2023-04-16 01:36:05 +0530

    p5

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -51,5 +51,33 @@ const loginController = async (req, res) => {
     res.status(500).send({ message: `Error in Login CTRL ${error.message}` });
   }
 };
+const authController = async(req,res) =>{
+try{
+  const user = await userModel.findOne({_id: req.body.userId});
+  if(!user){
+    return res.status(200).send({
+      message:"user not found",
+      success:false,
+    });
 
-module.exports = { loginController, registerController };
+  }else{
+    res.status(200).send({
+      success:true,
+      data:{
+        name:user.name,
+        email:user.email,
+      },
+
+    });
+  }
+}
+}
+  catch(error){
+  console.log(error);
+  res.status(500).send({
+    message:"auth error",
+    success:false,
+    error,
+  });
+}
+module.exports = { loginController, registerController , authController };
