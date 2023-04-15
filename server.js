81f7aefb607fff991cfda540d8c9db30b3e3f13b
diyaag2004 <diyaag2020@gmail.com>
2023-04-15 20:36:23 +0530

    a2

--- server.js
+++ server.js
@@ -2,22 +2,14 @@ const express = require("express");
 const colors = require("colors");
 const moragan = require("morgan");
 const dotenv = require("dotenv");
-const connectDB = require("./config/db");
-
 
 dotenv.config();
-
-
-connectDB();
 
 const app = express();
 
 
 app.use(express.json());
 app.use(moragan("dev"));
-
-
-app.use("/api/v1/user", require("./routes/userRoutes"));
 
 app.get("/",(req,res) => {
   res.status(200).send({
