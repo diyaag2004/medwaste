32e5aee4eb8c7e1999517e9eb9059ebf64626390
diyaag2004 <diyaag2020@gmail.com>
2023-04-16 00:14:46 +0530

    p3

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from 'react';
+import"../styles/RegisterStyles.css";
+import { Form, Input, message, checkbox, email } from "antd";
 
 function Login() {
   return (
     <div>
         <h1>login Page</h1>
+        <div className="form-container ">
+        <Form
+          layout="vertical"
+          onFinish={onfinishHandler}
+          className="login-form"
+        >
+          <h3 className="text-center">login From</h3>
+          
+          <Form.Item label="Email" name="email">
+            <Input type="email" required />
+          </Form.Item>
+          <Form.Item label="Password" name="password">
+            <Input type="password" required />
+          </Form.Item>
+        
+         
+          <Link to="/register" className="m-2">
+            Not a user Register here
+          </Link>
+          <button className="btn btn-primary" type="submit">
+            Login
+          </button>
+        </Form>
+      </div>
     </div>
   )
 }
